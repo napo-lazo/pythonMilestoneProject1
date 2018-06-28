0284aaa06a031ae417f94795295fb0b8e2fdb0bd
napoleon lazo <napoleon.lazo@hotmail.com>
2018-06-27 20:58:09 -0500

    players can take turns to place characters on the board

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,6 @@
-def drawBoard(cell):
-	cellValues[cell - 1] = "X"
+import random 
+
+def drawBoard():
 	
 	print("   |   |   ")
 	print(f" {cellValues[6]} | {cellValues[7]} | {cellValues[8]} ")
@@ -16,22 +17,33 @@ def drawBoard(cell):
 def setPlayers(players):
 
 	while players[0] != "X" and players[0] != "O":
-		players[0] = input("Player 1 do you wanna be X or O?").upper()
+		players[0] = input("Player 1 do you wanna be X or O? \n").upper()
 		if players[0] != "X" and players[0] != "O":
-			print("That is not a valid option")
+			print("That is not a valid option \n")
 	
 	if players[0] == "X":
 		players[1] = "O"
 	else:
 		players[1] = "X"
 
-	print(f"Player 1 is {players[0]}")
+	print(f"\nPlayer 1 is {players[0]}")
 	print(f"Player 2 is {players[1]}")
 
 
 cellValues = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 players = ["", ""]
+stillPlaying = True
 
 setPlayers(players)
-cell = int(input())
-drawBoard(cell)
+currentPlayer = random.randrange(0,2)
+print(f"\nPlayer {currentPlayer + 1} goes first\n")
+drawBoard()
+
+while stillPlaying:
+	cell = int(input(f"\nPlayer {currentPlayer + 1} where do you want to place your {players[currentPlayer]}?\n"))
+	cellValues[cell - 1] = players[currentPlayer]
+	drawBoard()
+	if currentPlayer == 0:
+		currentPlayer = 1
+	else:
+		currentPlayer = 0 
